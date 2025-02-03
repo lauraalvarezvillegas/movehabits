f44e67d20cf90f2241f4805497e05897b5d86b7d
lauraalvarez <lauav99@gmail.com>
2025-02-03 12:42:57 +0100

    workout item ui finished

--- app/src/main/java/com/lauraalvarez/movehabits/ui/workouts/WorkoutItem.kt
+++ app/src/main/java/com/lauraalvarez/movehabits/ui/workouts/WorkoutItem.kt
@@ -1,68 +1,109 @@
 package com.lauraalvarez.movehabits.ui.workouts
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.Timestamp
 import com.lauraalvarez.movehabits.R
 import com.lauraalvarez.movehabits.data.enums.ExerciseType
 import com.lauraalvarez.movehabits.data.model.Workout
 import com.lauraalvarez.movehabits.utils.DateTimeUtils.formatFirebaseDate
 import com.lauraalvarez.movehabits.utils.DateTimeUtils.formatFirebaseTime
-
+import java.util.Date
 
 @Composable
 fun WorkoutItem(workout: Workout) {
-
     Card(
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 6.dp,
-        ),
-        modifier = Modifier.size(width = 350.dp, height = 200.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+        modifier = Modifier
+            .size(width = 350.dp, height = 100.dp)
+            .border(
+                0.5.dp,
+                color = colorResource(id = R.color.original_blue),
+                RoundedCornerShape(26.dp)
+            ),
     ) {
-        //icon
-        Row {
-            //icon
-            if (workout.type == ExerciseType.STRENGTH.name) {
-                Icon(
-                    painter = painterResource(id = R.drawable.workout_icon),
-                    contentDescription = stringResource(R.string.strength_training_icon),
-                    modifier = Modifier.size(50.dp)
-                )
-            } else {
-                Icon(
-                    painter = painterResource(id = R.drawable.profile_icon),
-                    contentDescription = stringResource(R.string.cardio_training_icon),
-                    modifier = Modifier.size(50.dp)
-                )
-            }
-            Column {
-                //fecha
-                val date = formatFirebaseDate(workout.dateTime)
-                Text(text = date)
-                //hora
-                val time = formatFirebaseTime(workout.dateTime)
-                Text(text = time)
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White, shape = RoundedCornerShape(26.dp))
+                .padding(horizontal = 16.dp, vertical = 10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(
+                    id = if (workout.type == ExerciseType.STRENGTH.name)
+                        R.drawable.strength_icon
+                    else
+                        R.drawable.running_icon
+                ),
+                contentDescription = stringResource(
+                    if (workout.type == ExerciseType.STRENGTH.name)
+                        R.string.strength_training_icon
+                    else
+                        R.string.cardio_training_icon
+                ),
+                modifier = Modifier
+                    .size(70.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(text = formatFirebaseDate(workout.dateTime), fontSize = 16.sp)
+                Text(text = formatFirebaseTime(workout.dateTime), fontSize = 16.sp)
             }
 
-            //icon goto or done
-            if (workout.completed) {
-                //Icon( painter = painterResource(id = R.drawable.home_icon), contentDescription = stringResource(R.string.done_icon), modifier = Modifier.size(50.dp))
-            }else {
-                //Icon( painter = painterResource(id = R.drawable.home_icon), contentDescription = stringResource(R.string.go_to_icon), modifier = Modifier.size(50.dp))
-            }
-
-
+            Icon(
+                painter = painterResource(
+                    id = if (workout.completed)
+                        R.drawable.check_icon
+                    else
+                        R.drawable.right_icon
+                ),
+                contentDescription = stringResource(
+                    if (workout.completed)
+                        R.string.finished_workout_icon
+                    else
+                        R.string.strength_training_icon
+                ),
+                modifier = Modifier
+                    .size(40.dp)
+                    .align(Alignment.CenterVertically)
+            )
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun WorkoutItemPreview() {
+    val sampleWorkout = Workout(
+        userId = "123",
+        type = ExerciseType.CARDIO.name,
+        dateTime = Timestamp(Date()),
+        totalDurationSec = 3600,
+        restSec = 60,
+        completed = false,
+        exercises = emptyList()
+    )
+
+    WorkoutItem(workout = sampleWorkout)
+}
